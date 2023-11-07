e833fcfa7ac9e945235861126588e4758d7066b7
Hua Xiao <huaxiao666@gmail.com>
2023-11-07 00:27:06 -0600

    update all files

---
+++ app/src/main/java/com/example/mycoffeelist/CoffeeAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.mycoffeelist
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class CoffeeAdapter(private val coffeeImgList: List<String>, private val coffeeTitleList: List<String>,private val coffeeDescList: List<String>) : RecyclerView.Adapter<CoffeeAdapter.ViewHolder>(){
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val coffeeImage: ImageView
+        val coffeeTitle: TextView
+        val coffeeDescription: TextView
+
+        init {
+            // Find our RecyclerView item's ImageView for future use
+            coffeeImage = view.findViewById(R.id.coffee_image)
+            coffeeTitle = view.findViewById(R.id.coffee_title)
+            coffeeDescription = view.findViewById(R.id.coffee_description)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.coffee, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = coffeeImgList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.coffeeTitle.text = coffeeTitleList [position]
+        holder.coffeeDescription.text = coffeeDescList[position]
+        Glide.with(holder.itemView)
+            .load(coffeeImgList[position])
+            .centerCrop()
+            .into(holder.coffeeImage)
+
+    }
+}
--- app/src/main/java/com/example/mycoffeelist/MainActivity.kt
+++ app/src/main/java/com/example/mycoffeelist/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.mycoffeelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var coffeeImgList: MutableList<String>
+    private lateinit var rvCoffee: RecyclerView
+
+    // added features different from lab
+    private lateinit var coffeeTitleList: MutableList<String>
+    private lateinit var coffeeDescriptionList: MutableList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvCoffee = findViewById(R.id.coffee_list)
+        coffeeImgList = mutableListOf()
+        coffeeTitleList = mutableListOf()
+        coffeeDescriptionList = mutableListOf()
+
+        getCoffeeImageURL()
+    }
+
+    private fun getCoffeeImageURL() {
+        val client = AsyncHttpClient()
+
+        client["https://api.sampleapis.com/coffee/hot/", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
+                Log.d("Coffee", "response successful")
+                val coffeeArray = json.jsonArray
+                Log.d("coffee image link", coffeeArray.getJSONObject(1).getString("image"))
+
+                for (i in 1 until coffeeArray.length()) {
+                    coffeeImgList.add(coffeeArray.getJSONObject(i).getString("image"))
+                    coffeeTitleList.add(coffeeArray.getJSONObject(i).getString("title"))
+                    coffeeDescriptionList.add(coffeeArray.getJSONObject(i).getString("description"))
+
+                    val adapter = CoffeeAdapter(coffeeImgList, coffeeTitleList, coffeeDescriptionList)
+                    rvCoffee.adapter = adapter
+                    rvCoffee.layoutManager = LinearLayoutManager(this@MainActivity)
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Coffee Error", errorResponse)
+            }
+        }]
     }
 }
